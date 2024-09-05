e3c8d9b0def8e9e1eb94d95c3cef931d1176f705
lorirothermel <lori@rothermel1.com>
2024-09-04 21:17:07 -0400

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Lori Rothermel on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

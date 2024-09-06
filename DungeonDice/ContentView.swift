7f06f2c00b6b7a78dde4a52d3d5fc9a2e41aa032
lorirothermel <lori@rothermel1.com>
2024-09-05 20:34:19 -0400

    Chapter 4.4 LazyVGrid for buttons

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var resultMessage = ""
-        
+    
     
     enum Dice: Int, CaseIterable {
         case four = 4
@@ -27,76 +27,33 @@ struct ContentView: View {
     
     
     var body: some View {
-        VStack {
-            Text("Dungeon Dice")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .foregroundColor(.red)
-            
-            Spacer()
-            
-            Text(resultMessage)
-                .font(.largeTitle)
-                .fontWeight(.medium)
-                .frame(height: 150)
-                .multilineTextAlignment(.center)
-            
-            Spacer()
-            
-//            Group {
-//                HStack {
-//                    Button("\(Dice.four.rawValue)-sided") {
-//                        resultMessage = "You Rolled a \(Dice.four.roll()) on a \(Dice.four.rawValue)-sided Dice."
-//                    }  // Button
-//                    Spacer()
-//                    Button("\(Dice.six.rawValue)-sided") {
-//                        resultMessage = "You Rolled a \(Dice.six.roll()) on a \(Dice.six.rawValue)-sided Dice."
-//                    }  // Button
-//                    Spacer()
-//                    Button("\(Dice.eight.rawValue)-sided") {
-//                        resultMessage = "You Rolled a \(Dice.eight.roll()) on a \(Dice.eight.rawValue)-sided Dice."
-//                    }  // Button
-//                }  //HStack
-//                
-//                HStack {
-//                    Button("\(Dice.ten.rawValue)-sided") {
-//                        resultMessage = "You Rolled a \(Dice.ten.roll()) on a \(Dice.ten.rawValue)-sided Dice."
-//                    }  // Button
-//                    Spacer()
-//                    Button("\(Dice.twelve.rawValue)-sided") {
-//                        resultMessage = "You Rolled a \(Dice.twelve.roll()) on a \(Dice.twelve.rawValue)-sided Dice."
-//                    }  // Button
-//                    Spacer()
-//                    Button("\(Dice.twenty.rawValue)-sided") {
-//                        resultMessage = "You Rolled a \(Dice.twenty.roll()) on a \(Dice.twenty.rawValue)-sided Dice."
-//                    }  // Button
-//                }  //HStack
-//                
-//                Button("\(Dice.hundred.rawValue)-sided") {
-//                    resultMessage = "You Rolled a \(Dice.hundred.roll()) on a \(Dice.hundred.rawValue)-sided Dice."
-//                }  // Button
-//                
-//            }  // Group
-//            .buttonStyle(.borderedProminent)
-//            .tint(.red)
-//            .padding(.leading)
-//            .padding(.trailing)
-            
-            Group {
-                
-                ForEach(Dice.allCases, id: \.self) { die in
-                    Button("\(die.rawValue)-sided") {
-                        resultMessage = "You rolled a \(die.roll()) on a \(die.rawValue)-sided dice."
-                    }  // Button
-                }  // ForEach
-                
-            }  // Group
-            .buttonStyle(.borderedProminent)
-            .tint(.red)
-                       
-            
-        }  // VStack
-        .padding()
+        
+        Text("Dungeon Dice")
+            .font(.largeTitle)
+            .fontWeight(.black)
+            .foregroundColor(.red)
+          
+        Text(resultMessage)
+            .font(.largeTitle)
+            .fontWeight(.medium)
+            .frame(height: 150)
+            .multilineTextAlignment(.center)
+        
+        Spacer()
+        
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))], content: {
+            ForEach(Dice.allCases, id: \.self) { die in
+                Button("\(die.rawValue)-sided") {
+                    resultMessage = "You rolled a \(die.roll()) on a \(die.rawValue)-sided dice."
+                }  // Button
+            }  // ForEach
+        })  // LazyVGrid
+        .buttonStyle(.borderedProminent)
+        .tint(.red)
+        
+        
+        
+        
     }  // some View
 }  // ContentView
 

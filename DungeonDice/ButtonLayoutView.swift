a3ceb9e3e61f6dca07f62ebdfabe92aa15f80072
lorirothermel <lori@rothermel1.com>
2024-09-06 23:11:34 -0400

    Chapter 4.7 PreferenceKeys, GeometryReader and Extracted SubViews

---
+++ DungeonDice/ButtonLayoutView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonLayoutView.swift
+//  DungeonDice
+//
+//  Created by Lori Rothermel on 9/6/24.
+//
+
+import SwiftUI
+
+struct ButtonLayoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonLayoutView()
+}
